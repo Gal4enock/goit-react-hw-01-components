f075ffd224ff821c5cc7c8c235b0bfc13043fd9b
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-11-20 10:11:59 +0200

    исправляет статус

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const FriendList = ({ friends }) => (
   <>
-    {friends.map(({ avatar, name, isonline, id }) => {
-    console.log(isonline);
+    {friends.map(({ avatar, name, isOnline, id }) => {
+    console.log(isOnline);
      return( <li key={id} className="item">
-       <span className={isonline ? "status" : "status-non"}></span> 
+       {/* {isOnline ? <span className="status"></span> : <span className="status-non"></span>} */}
+       <span className={isOnline ? "status" : "status-non"}></span>
         <img className="avatar" src={avatar} alt={name} width="48" />
         <p className="name">{name}</p>
       </li>)
